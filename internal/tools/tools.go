5d7ede390607e9c9d5a0435ac4639658e6207f5a
Mario Carrion <mario@carrion.ws>
2024-07-08 23:13:46 -0400

    fix(gomod): Use `oapi-codegen/oapi-codegen`
    Replacing `deepmap/oapi-codegen`, and version updated.

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -1,12 +1,12 @@
 package tools
 
 import (
-	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"    // OpenAPI 3 Code Generator
-	_ "github.com/fdaines/spm-go"                           // Software Package Metrics
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint" // Linter
-	_ "github.com/jackc/tern/v2"                            // Database Migration
-	_ "github.com/maxbrunsfeld/counterfeiter/v6"            // Mock/Spies/Stubs
-	_ "github.com/sqlc-dev/sqlc/cmd/sqlc"                   // Type-Safe SQL generator
-	_ "goa.design/model/cmd/mdl"                            // Structurizer
-	_ "goa.design/model/cmd/stz"                            // Structurizer
+	_ "github.com/fdaines/spm-go"                                // Software Package Metrics
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"      // Linter
+	_ "github.com/jackc/tern/v2"                                 // Database Migration
+	_ "github.com/maxbrunsfeld/counterfeiter/v6"                 // Mock/Spies/Stubs
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen" // OpenAPI 3 Code Generator
+	_ "github.com/sqlc-dev/sqlc/cmd/sqlc"                        // Type-Safe SQL generator
+	_ "goa.design/model/cmd/mdl"                                 // Structurizer
+	_ "goa.design/model/cmd/stz"                                 // Structurizer
 )
